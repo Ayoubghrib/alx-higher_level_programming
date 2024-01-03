a38cac1e14d12a2a6eb2222ed55cabccbb9182c1
Ayoubghrib <Ayoubgharib30@gmail.com>
2024-01-03 14:31:07 -0800

    python

--- 0x01-python-if_else_loops_functions/100-print_tehbapla.py
+++ 0x01-python-if_else_loops_functions/100-print_tehbapla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0:
-        diff = 0
-    else:
-        diff = 32
-    print('{}'.format(chr(i - diff)), end='')
 
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        a = i
+    else:
+        a = i - 32
+    print("{}".format(chr(a)), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            i = chr(ord(i) - 32)
-        print("{}".format(i), end="")
+            c = chr(ord(i) - 32)
+        else:
+            c = i
+        print("{}".format(c), end="")
     print()
-
